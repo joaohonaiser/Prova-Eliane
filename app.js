29abe8bf42c00a5b3bec79719318d8259e58d3b6
JoaoHonaiser <joao.honaiser@escola.pr.gov.br>
2026-06-23 08:31:16 -0300

    novos produtos da Glitch Productions adicionados a loja

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ const PRODUCTS_DATA = [
     { id: 21, title: "Mangá Kaoru Hana Wa rin to saku - Volume 1", category: "Mangá", price: 59.80, image: "Kaoru.webp" },
     { id: 22, title: "Mangá Demon Slayer - Volume 1", category: "Mangá", price: 39.99, image: "Demon slayer.jpg" },
     { id: 23, title: "Mangá Jujutso Kaisen 0 - Edição Especial", category: "Mangá", price: 199.90, image: "0.jpeg" },
-    { id: 24, title: "Mangá One Piece - Volume 2", category: "Mangá", price: 45.70, image: "One Piece Vol -2.jpg" }
+    { id: 24, title: "Mangá One Piece - Volume 2", category: "Mangá", price: 45.70, image: "One Piece Vol -2.jpg" },
+    { id: 25, title: "Jax Floppy Hat", category: "Glitch", price: 55.00, image: "JaxFloppyHat4.webp" },
+    { id: 26, title: "Bubble Hoodie", category: "Glitch", price: 145.00, image: "Bubble.webp" },
+    { id: 27, title: "Jax Hooded Jacket", category: "Glitch", price: 150.00, image: "JaxBlusaa.webp" },
 ];
 
 const state = {
